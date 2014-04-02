055ee3a951f95734667631fe2204e630b485876d
Matthew Brener <akkatracker@gmail.com>
2014-04-02 22:23:52 +1100

    Initial Commit
    Pushed files

---
+++ biv.py
@@ -0,0 +1,16 @@
+print("Bivouac Logging Tool. \nCopyright Matthew Brener 2014")
+exercise = input("What exercise is this? ")
+if exercise == "Navex":
+    print("You chose Navex")
+    section = input("Section: ")
+    while section.lower() != "finish":
+        report = input("Report type: ")
+        if report.upper() == "SITREP":
+            #Sitrep Details ALPHA BRAVO etc.
+        elif report.upper() == "CASEVAC":
+            #Casevac Details here
+        elif report.upper() == "CAPTURE":
+    
+        
+        
+        
