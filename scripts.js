d02d0ec291f7c8e6ad05ef3311b0a0b5cc2a886f
Mitchell McMillan <mitchelldmcmillan@gmail.com>
2014-04-11 15:38:04 +1000

    Sitrep function working

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function sitrep(){
+	var output = document.getElementById('output').value,
+		time = new Date(),
+		time = String(time),
+		time = time.substring(0, 21),
+		alpha = document.getElementById('sitrep_callsign').value,
+		bravo = document.getElementById('sitrep_grid').value,
+		charlie = document.getElementById('sitrep_objective').value,
+		delta = document.getElementById('sitrep_supplies').value,
+		echo = document.getElementById('sitrep_equipment').value,
+		foxtrot = document.getElementById('sitrep_morale').value,
+		golf = document.getElementById('sitrep_comments').value,
+		out = time + " | SITREP | Callsign:" + alpha + "| Location:" + bravo + "| Current Objective:" + charlie + "| Level of Supplies:" + delta + "| Equipment Status:" + echo + "| Morale: " + foxtrot + "| Comments: "+ golf +'\n';
+	document.getElementById('output').value = output  + out;
+};
